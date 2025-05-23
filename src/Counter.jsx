1b4c6eb10400836df21d8e4038712bf9a0c59503
Ankit-Hub-Max <ankitsingh623@gmail.com>
2025-05-24 00:30:49 +0530

    latest commit

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,7 +5,7 @@ function Counter(){
 return(
 <div>
     <h1>Counter App</h1>
-  <p>Count:{count}</p>
+  <p>Count: {count}</p>
   <button onClick={()=>setCount(count+1)}>Increment</button>
   <button onClick={()=>setCount(count-1)}>Decrement</button>
 </div>
